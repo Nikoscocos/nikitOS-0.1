aa125e23b0820af090401f1880e608a3e69f5ffe
Nikoscocos <77122703+Nikoscocos@users.noreply.github.com>
2021-10-24 21:07:11 +0300

    Update freezecore.js

--- core/freezecore.js
+++ core/freezecore.js
@@ -446,6 +446,9 @@ function minimizeDefWindow(windowid) {
 function dragWindow(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.getElementById("header" + elmnt.id)) {
+      document.ontouchstart = function() {
+        document.getElementById("header" + elmnt.id).ontouchmove = dragMouseDown;
+      }
       document.getElementById("header" + elmnt.id).onmousedown = dragMouseDown;
     } else {alert(elmnt)}
   
@@ -453,6 +456,12 @@ function dragWindow(elmnt) {
       e = e || window.event;
       pos3 = e.clientX;
       pos4 = e.clientY;
+      if (pos3==undefined) {
+        pos3 = e.touches[0].clientX;
+      }
+      if (pos4==undefined) {
+        pos4 = e.touches[0].clientY;
+      }
       document.onmouseup = closeDragElement;
       document.onmousemove = elementDrag;
     }
@@ -463,6 +472,14 @@ function dragWindow(elmnt) {
       pos2 = pos4 - e.clientY;
       pos3 = e.clientX;
       pos4 = e.clientY;
+      if (pos3==undefined) {
+        pos3 = e.touches[0].clientX;
+        pos1 = pos3 - e.touches[0].clientX;
+      }
+      if (pos4==undefined) {
+        pos4 = e.touches[0].clientY;
+        pos2 = pos4 - e.touches[0].clientY;
+      }
       windowid = elmnt.id
       if (document.getElementById(windowid).style.width == "calc(100% - 60px)") {
         if (document.getElementById(windowid).style.height == "100%") {
