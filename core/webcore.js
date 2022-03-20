2eff947e66096dbac2330009787ac79c08036589
Nikoscocos <77122703+Nikoscocos@users.noreply.github.com>
2022-03-20 21:38:34 +0400

    Update webcore.js

--- core/webcore.js
+++ core/webcore.js
@@ -99,7 +99,8 @@ function openWindow(wtitie, content, icon, maxwidth, maxheight) {
     maxwin = " onclick='maximizeWindow(" + '"' + windowid + '"' + ")'"
     minwin = " onclick='minimizeWindow(" + '"' + windowid + '"' + ")'"
     windowidclc = "'" + windowid + "'"
-    titleicon = icon.replace(/32/g, '17')
+    titleicon = icon.replace(/40/g, '17')
+    icon = icon.replace(/40/g, '32')
     elementto = '<div class="window" id="' + windowid + '" onmousedown="markWindow(' + windowidclc + ')">' + 
                 '<div id="' + windowhid + '" ' + suksuk + ' class="windowheader">' + titleicon + '&nbsp;<b>' + wtitie + '</b><p ' + closewin + ' style="margin: auto; margin-right: 7px; margin-top: -20px; width: 10px; text-align: right;"> <i class="fa fa-window-close" aria-hidden="true"></i></p><p ' + maxwin + ' style="margin: auto; margin-right: 25px; margin-top: -21px; width: 10px; text-align: right;"> <i class="fa fa-window-maximize" aria-hidden="true"></i></p><p ' + minwin + ' style="margin: auto; margin-right: 43px; margin-top: -21px; width: 10px; text-align: right;"> <i class="fa fa-window-minimize" aria-hidden="true"></i></p></div><div class="windowblock">' 
                 + content + '</div>' + '</div>';
@@ -149,7 +150,8 @@ function openCustomWindow(wtitie, content, icon, maxwidth, maxheight) {
     maxwin = " onclick='maximizeWindow(" + '"' + windowid + '"' + ")'"
     minwin = " onclick='minimizeWindow(" + '"' + windowid + '"' + ")'"
     windowidclc = "'" + windowid + "'"
-    titleicon = icon.replace(/32/g, '17')
+    titleicon = icon.replace(/40/g, '17')
+    icon = icon.replace(/40/g, '32')
     elementto = '<div class="mywindow" id="' + windowid + '" onmousedown="markWindow(' + windowidclc + ')">' + 
                 '<div id="' + windowhid + '" ' + suksuk + ' class="windowheader">' + titleicon + '&nbsp;<b>' + wtitie + '</b><p ' + closewin + ' style="margin: auto; margin-right: 7px; margin-top: -20px; width: 10px; text-align: right;"> <i class="fa fa-window-close" aria-hidden="true"></i></p><p ' + maxwin + ' style="margin: auto; margin-right: 25px; margin-top: -21px; width: 10px; text-align: right;"> <i class="fa fa-window-maximize" aria-hidden="true"></i></p><p ' + minwin + ' style="margin: auto; margin-right: 43px; margin-top: -21px; width: 10px; text-align: right;"> <i class="fa fa-window-minimize" aria-hidden="true"></i></p></div><div class="onwindowcontent">' 
                 + content + '</div></div>' + '</div>';
