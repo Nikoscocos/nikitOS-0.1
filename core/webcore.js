0c21160c3ca76e348f89dc000ffe858826b71ec8
Nikoscocos <77122703+Nikoscocos@users.noreply.github.com>
2022-03-21 20:17:10 +0400

    Update webcore.js

--- core/webcore.js
+++ core/webcore.js
@@ -609,4 +609,33 @@ function reload() {
     closeAllWindows()
     preloader()
 }
+function zero_first_format(value)
+    {
+        if (value < 10)
+        {
+            value='0'+value;
+        }
+        return value;
+    }
+
+    /* функция получения текущей даты и времени */
+    function sidebardater()
+    {
+        var current_datetime = new Date();
+        var day = zero_first_format(current_datetime.getDate());
+        var month = zero_first_format(current_datetime.getMonth()+1);
+        var year = current_datetime.getFullYear();
+        var hours = zero_first_format(current_datetime.getHours());
+        var minutes = zero_first_format(current_datetime.getMinutes());
+        var seconds = zero_first_format(current_datetime.getSeconds());
+
+        return hours+":"+minutes+"<br>"+day+"."+month+"."+year;
+    }
+    try {
+        document.getElementById('curtime').innerHTML = date_time();
+    }
+    catch(e) {}
+function addlog(logtext) {
+    return false;
+}
 window.onload = preloader()
